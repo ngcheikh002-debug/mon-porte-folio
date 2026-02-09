aa220f0d79fce6e1898588560f12331965c1e59e
ngcheikh002-debug <ngcheikh002@gmail.com>
2026-02-09 19:34:48 +0000

    Add files via upload

---
+++ portefolioenligne.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+st.set_page_config( page_title="Portfolio – CHEIKH NIANG", )
+with st.sidebar:
+    st.title("NAVIGATION")
+    st.image("image.jpg",width=150)
+    st.subheader("Cheikh NIANG")
+    st.write("📞 Téléphone:76 140 27 01")
+    st.write("📧  Email:ngcheikh002@gmail.com")
+    st.write("📍 Localisation :pikine Dakar, Sénégal")
+    
+st.header("MON PROFIL")
+st.write("Titulaire d’une formation en géographie et en géomatique, rigoureux et motivé, disposant de solides bases en analyse spatiale, cartographie et outils numériques. Doté d’un bon esprit d’analyse, d’une capacité d’adaptation rapide et d’un sens de l’organisation, je souhaite mettre mes compétences au service d’une structure professionnelle tout en contribuant efficacement à ses objectifs")
+
+st.header("mes competences")
+st.write("Maîtrise des outils QGIS et ArcGIS pour la collecte, le traitement et l’analyse de données spatiales. Réalisation de cartes thématiques, analyses spatiales et interprétation de résultats pour l’aide à la décision territoriale.")
+st.write("Conception de cartes thématiques claires et exploitables, analyse de phénomènes géographiques et interprétation des dynamiques spatiales pour appuyer la planification et l’aménagement du territoire.")
+st.write("Structuration, exploitation et contrôle de qualité de bases de données géographiques, avec rigueur dans le traitement, l’analyse et la mise à jour des données spatiales")
+st.write("Esprit d’analyse et de synthèse, sens de l’organisation et respect des délais. Capacité à travailler en autonomie ou en équipe avec une communication claire et professionnelle")
+st.write("Maîtrise de la suite bureautique : Word, Excel, PowerPoint")
+
+
+st.header("PARCOURS ACADEMIQUES")
+st.write("BTS EN GEOMATIQUE/CEDT G15/EN COURS")
+st.write("Licence en GEOGRAPHIE/UCAD/2025")
+st.write("BACCALAUREAT L2/LYCEE SLL/2022")
+
+st.header("PROJETS et RÉALISATIONS")
+st.subheader("Projets")
+st.write("Objectif : Analyser l’organisation spatiale et l’accessibilité d’un territoire")
+st.write("Méthode : Analyse spatiale")
+st.write("Outils : QGIS")
+st.write("Résultat : Carte thématique et interprétation géographique")
+
+st.subheader("Expérience Terrain")
+st.write("Collecte de donnéesplus precisemment des points topographiques")
+st.write("Travaux pratiques universitaires")
+st.write("Traitement et analyse des données collectées")
+
+st.header("outils et logiciels")
+st.write("ARCGIS")
+st.write("QGIS")
+st.write("Google Earth")
+st.write("python(SIG)")
+
